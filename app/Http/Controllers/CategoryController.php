834a3f2ef942b6c9a35323ff5f8d673816b26e3e
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-09-01 08:05:54 +0800

    implemented listing of product category pagination

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\EndCategory;
 use App\Models\MidCategory;
+use App\Models\Product;
 use App\Models\TopCategory;
 use Illuminate\Http\Request;
 
@@ -14,18 +15,27 @@ class CategoryController extends Controller
         $category_products = [];
 
         if ($level === "top_category") {
-            $category_products = TopCategory::with("midCategories.endCategories.products")->findOrFail($id);
-            $category_products = $category_products->midCategories->flatMap->endCategories->flatMap->products;
+            $topCategory = TopCategory::with("midCategories.endCategories.products")->findOrFail($id);
+            $productIds = $topCategory->midCategories->flatMap
+                                    ->endCategories->flatMap
+                                    ->products->pluck("id");
+
+            $category_products = Product::whereIn('id', $productIds)->paginate(6);
         }
 
         if ($level === "mid_category") {
             $category_products = MidCategory::with("endCategories.products")->findOrFail($id);
-            $category_products = $category_products->endCategories->flatMap->products;
+            $productIds = $category_products->endCategories->flatMap
+                                ->products->pluck("id");
+
+            $category_products = Product::whereIn('id', $productIds)->paginate(6);
         }
 
         if ($level === "end_category") {
-            $category_products = EndCategory::with("products")->findOrFail($id);
-            $category_products = $category_products->products;
+            $category_products = EndCategory::with("products")->findOrFail($id); 
+            $productIds = $category_products->products->pluck("id");
+
+            $category_products = Product::whereIn('id', $productIds)->paginate(6);
         }
 
 
