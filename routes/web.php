79b0bac94c9bc0f1dc674fdd8c87023790a7e297
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-09-13 10:54:17 +0800

    refactored the code, group all route that uses the common middleware

--- routes/web.php
+++ routes/web.php
@@ -24,32 +24,34 @@ Route::get("/contactUs", [ContactUsController::class, "contactUs"])->name("conta
 Route::get("/aboutUs", [AboutUsController::class, "aboutUs"])->name("aboutUs");
 Route::get("/cart", [CartController::class, "cart"])->name("cart");
 
+Route::middleware("guest:customer")->group(function () {
+    Route::get("/login", [LoginController::class, "login"])->name("login");
+    Route::post("/login", [LoginController::class, "loginCustomer"])->name("login.customer");
 
-Route::get("/login", [LoginController::class, "login"])->name("login");
-Route::post("/login", [LoginController::class, "loginCustomer"])->name("login.customer");
+    Route::get("/register", [RegisterController::class, "register"])->name("register");
+    Route::post("/store", [RegisterController::class, "store"])->name("register.store");
+});
 
 Route::post("/logout", [LoginController::class, "logoutCustomer"])->name("logout.customer");
-
-
-Route::get("/register", [RegisterController::class, "register"])->name("register");
-Route::post("/store", [RegisterController::class, "store"])->name("register.store");
 
 Route::get("/level/{level}/category/{id}/value/{value}", [CategoryController::class, "index"])->name("category.index");
 
 Route::get("/products/{id}", [ProductController::class, "show"])->name("product.show");
 Route::get("/search", [ProductController::class, "search"])->name("product.search");
 
-
 Route::get("/cart", [CartController::class, "index"])->name("cart.index");
 Route::post("/cart/product/add/{id}", [CartController::class, "add"])->name("cart.add");
 Route::delete("/cart/product/destroy/{id}", [CartController::class, "destroy"])->name("cart.destroy");
 Route::post("/cart/product/update{id}", [CartController::class, "update"])->name("cart.update");
 
-Route::get("/checkout", [CheckoutController::class, "checkout"])->middleware("auth:customer")->name("checkout.index");
-Route::delete("/checkout/{id}", [CheckoutController::class, "destroy"])->middleware("auth:customer")->name("checkout.destroy");
-Route::post("/checkout/place-order", [CheckoutController::class, "placeOrder"])->middleware("auth:customer")->name("checkout.placeOrder");
 
-Route::get("/dashboard", [Dashboard::class, "index"])->middleware("auth:customer")->name("dashboard.index");
-Route::post("/dashboard/update-profile", [Dashboard::class, "updateProfile"])->middleware("auth:customer")->name("dashboard.update-profile");
-Route::post("/dashboard/update-address", [Dashboard::class, "updateAddress"])->middleware("auth:customer")->name("dashboard.update-address");
-Route::post("/dashboard/update-password", [Dashboard::class, "updatePassword"])->middleware("auth:customer")->name("dashboard.update-password");
+Route::middleware("auth:customer")->group(function() {
+    Route::get("/checkout", [CheckoutController::class, "checkout"])->name("checkout.index");
+    Route::delete("/checkout/{id}", [CheckoutController::class, "destroy"])->name("checkout.destroy");
+    Route::post("/checkout/place-order", [CheckoutController::class, "placeOrder"])->name("checkout.placeOrder");
+    
+    Route::get("/dashboard", [Dashboard::class, "index"])->name("dashboard.index");
+    Route::post("/dashboard/update-profile", [Dashboard::class, "updateProfile"])->name("dashboard.update-profile");
+    Route::post("/dashboard/update-address", [Dashboard::class, "updateAddress"])->name("dashboard.update-address");
+    Route::post("/dashboard/update-password", [Dashboard::class, "updatePassword"])->name("dashboard.update-password");
+});
