17ffb223e3077c6360f4cb186721c35d3a116de9
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-10-13 05:44:52 +0800

    implemented sort, limit, search and pagination in the subscribers table

--- app/Livewire/Admin/Panels/Subscribers/ManageSubscribersIndex.php
+++ app/Livewire/Admin/Panels/Subscribers/ManageSubscribersIndex.php
@@ -2,13 +2,55 @@
 
 namespace App\Livewire\Admin\Panels\Subscribers;
 
-
+use App\Models\Subscriber;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ManageSubscribersIndex extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = "bootstrap";
+    public $perPage = 10;
+    public $sortField = "id";
+    public $sortDirection = "desc";
+    public $search = "";
+
+    public function updatingSearch() {
+        $this->resetPage();
+    }
+
+    public function updatingPerPage() {
+        $this->resetPage();
+    }
+
+    public function sortBy($field) {
+        if ($this->sortField === $field) {
+            $this->sortDirection = $this->sortDirection === "asc" ? "desc" : "asc";
+        }
+        else {
+            $this->sortField = $field;
+            $this->sortDirection = "asc";
+        }
+    }
+
     public function render()
     {
-        return view('livewire.admin.panels.subscribers.manage-subscribers-index');
+        $subscribers = Subscriber::query()
+                            ->when($this->sortField == "id", fn($query) =>  
+                                $query->orderBy("id", $this->sortDirection)
+                            )
+                            ->when($this->sortField == "email", fn($query) =>  
+                                $query->orderBy("email", $this->sortDirection)
+                            )
+                            ->when($this->sortField == "is_verified", fn($query) =>  
+                                $query->orderBy("is_verified", $this->sortDirection)
+                            )
+                            ->when($this->search, fn($query) => 
+                                $query->where("email", "like", "%{$this->search}%")
+                            )
+                            ->paginate($this->perPage);
+
+        return view('livewire.admin.panels.subscribers.manage-subscribers-index', compact("subscribers"));
     }
 }
--- resources/views/livewire/admin/panels/subscribers/manage-subscribers-index.blade.php
+++ resources/views/livewire/admin/panels/subscribers/manage-subscribers-index.blade.php
@@ -15,53 +15,84 @@
     <div class="row mb-3">
         <div class="col-md-6 d-flex align-items-center">
             <label class="form-label me-2 mb-0">Show</label>
-            <select class="form-select form-select-sm w-auto">
-                <option>10</option>
-                <option>25</option>
-                <option>50</option>
+            <select wire:model.live="perPage" class="form-select form-select-sm w-auto">
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
             </select>
             <span class="ms-2">entries</span>
         </div>
         <div class="col-md-6 text-end">
-            <input type="text" class="form-control form-control-sm w-auto d-inline" placeholder="Search...">
+            <input wire:model.live.debounce.300ms="search"  type="text" class="form-control form-control-sm w-auto d-inline" placeholder="Search...">
         </div>
     </div>
 
                     
     <div class="table-responsive">
-        <table class="table table-bordered table-striped table-hover align-middle">
+        <table class="table table-striped table-hover align-middle">
             <thead class="table-light">
                 <tr>
-                    <th># <i class="bi bi-arrow-down-up ms-1 text-muted"></i></th>
-                    <th>Subscriber Email <i class="bi bi-arrow-down-up ms-1 text-muted"></i></th>
-                    <th>Status<i class="bi bi-arrow-down-up ms-1 text-muted"></i></th>
+                    <th scope="col">
+                        <div class="d-flex align-items-center">
+                            <span class="me-1">#</span>
+                            <button wire:click="sortBy('id')" class="btn btn-sm btn-link p-0 text-secondary">
+                                <i class="bi bi-arrow-down-up"></i>
+                            </button>
+                        </div>
+                    </th>
+                    <th scope="col">
+                        <div class="d-flex align-items-center">
+                            <span class="me-1">Subscriber Email</span>
+                            <button wire:click="sortBy('email')" class="btn btn-sm btn-link p-0 text-secondary">
+                                <i class="bi bi-arrow-down-up"></i>
+                            </button>
+                        </div>
+                    </th>
+                    <th scope="col">
+                        <div class="d-flex align-items-center">
+                            <span class="me-1">Status</span>
+                            <button wire:click="sortBy('is_verified')" class="btn btn-sm btn-link p-0 text-secondary">
+                                <i class="bi bi-arrow-down-up"></i>
+                            </button>
+                        </div>
+                    </th>
                     <th>Action</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>john.doe@mail.com</td>
-                    <td>Pending Vefication</td>
-                    <td>
-                        <a href="#" class="btn btn-sm btn-danger">
-                            <i class="bi bi-trash me-1"></i> Delete
-                        </a>
-                    </td>
-                </tr>
+                @forelse ($subscribers as $subscriber)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $subscriber->email }}</td>
+                        <td>
+                            <span class="{{ $subscriber->is_verified ? 'badge bg-success' : 'badge bg-danger' }}">
+                                {{ $subscriber->is_verified ? "Verified" : "Pending Verification" }}
+                            </span>
+                        </td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-danger">
+                                <i class="bi bi-trash me-1"></i> Delete
+                            </a>
+                        </td>
+                    </tr>         
+                @empty
+                    <tr>
+                        <td colspan="8" class="text-center p-5 text-muted">
+                            <i class="fa-solid fa-bell-slash me-2"></i> No emails found matching your criteria.
+                        </td>
+                    </tr>
+                @endforelse
             </tbody>
         </table>
     </div>
                 
-    <div class="d-flex justify-content-between align-items-center mt-2">
-        <small class="text-muted">Showing 1 to 10 of 20 entries</small>
+   <div class="d-flex justify-content-between align-items-center mt-3">
+        <small class="text-muted">
+            Showing {{ $subscribers->firstItem() }} to {{ $subscribers->lastItem() }} of {{ $subscribers->total() }} entries
+        </small>
+        
         <nav>
-            <ul class="pagination pagination-sm mb-0">
-                <li class="page-item disabled"><a class="page-link">Previous</a></li>
-                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
+            {{ $subscribers->links("pagination::livewire-bootstrap") }}
         </nav>
     </div>
 
